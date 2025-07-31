da308c0bdd602be70f7817483ab2b35794cc580b
wualice078 <wualice078@berkeley.edu>
2025-07-30 22:33:28 -0700

    room class

--- proj3/src/core/Room.java
+++ proj3/src/core/Room.java
@@ -1,4 +1,14 @@
 package core;
 
+import java.awt.*;
+import java.util.Random;
+
 public class Room {
+    private Point p1;
+    private Point p2;
+
+    public Room (Point p1, Point p2) {
+        this.p1 = p1;
+        this.p2 = p2;
+    }
 }
