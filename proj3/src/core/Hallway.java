df72273fd3d87eaac0b355b8c74c92c4e6b71934
wualice078 <wualice078@berkeley.edu>
2025-07-31 12:10:28 -0700

    hallway idea

--- proj3/src/core/Hallway.java
+++ proj3/src/core/Hallway.java
@@ -7,21 +7,38 @@ import java.util.Map;
 public class Hallway {
     private Point p1;
     private Point p2;
-    private Map<Point, Integer> turns;
+    private Point turnPoint;
+    private int direction;
 
     public Hallway(Point p1, Point p2) {
         this.p1 = p1;
         this.p2 = p2;
-        turns = new HashMap<Point, Integer>();
+        turnPoint = new Point(p1.x, p2.y);
+
+        if(p1.x < p2.x && p1.y > p2.y) {
+            this.direction = 1;
+        } else if(p1.x > p2.x && p1.y > p2.y) {
+            this.direction = 2;
+        } else if(p1.x > p2.x && p1.y < p2.y) {
+            this.direction = 3;
+        } else if(p1.x < p2.x && p1.y < p2.y) {
+            this.direction = 4;
+        }
     }
 
-    public Hallway(Point p1, Point p2, Map<Point, Integer> turns) {
-        this.p1 = p1;
-        this.p2 = p2;
-        this.turns = turns;
+    public int getDirection() {
+        return this.direction;
     }
 
-    public void addTurn(Point p, int d) {
-        this.turns.put(p, d);
+    public Point getTurnPoint() {
+        return this.turnPoint;
+    }
+
+    public Point getP1() {
+        return this.p1;
+    }
+
+    public Point getP2() {
+        return this.p2;
     }
 }
