78905713a0351ebb4bbd6001ef02ac636453a33c
Joyce1026 <13617531799@163.com>
2025-08-03 13:00:04 -0700

    adjustment to SaveLoad

--- proj3/src/core/Main.java
+++ proj3/src/core/Main.java
@@ -139,7 +139,7 @@ public class Main {
     }
 
     public static void loadGame() {
-        SaveWorld saved = SaveLoad.load();
+        SaveLoad saved = SaveLoad.load();
         if (saved == null) {
             displayMenu();
             return;
